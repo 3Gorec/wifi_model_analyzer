341900a960665937040e802743fa1d7883b594ea
Egor Dudyak <gggorec@gmail.com>
2016-01-27 23:09:08 +0600

    Initial commit

---
+++ src/wifi_model_analyzer/Wifi_model_analyzer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wifi_model_analyzer;
+
+/**
+ *
+ * @author gorec
+ */
+public class Wifi_model_analyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+  
+    
+}
